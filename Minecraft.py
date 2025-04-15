97d965ee4d544a3c38ed07a85130cc9eec9e3207
Mike Noble <jr_nobes2008@hotmail.com>
2025-04-15 11:14:26 -0500

    Defined key to destroy voxels. Fully replicated Ursina game demo.

--- Minecraft.py
+++ Minecraft.py
@@ -9,13 +9,16 @@ class Voxel(Button):
             model = 'cube',
             origin_y = 0.5,
             texture = 'white_cube',
-            color = color.white,
+            color = color.color(0,0,random.uniform(0.9,1)),
             highlight_color = color.lime)
 
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
                 voxel = Voxel(position = self.position + mouse.normal)
+
+            if key == 'right mouse down':
+                destroy(self)
 
 app = Ursina()
 
