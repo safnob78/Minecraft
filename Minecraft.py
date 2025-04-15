7be7f0102e5f6375ef1befeb114b1365532c37f9
Mike Noble <jr_nobes2008@hotmail.com>
2025-04-15 11:30:12 -0500

    Created custom voxels with custom textures

--- Minecraft.py
+++ Minecraft.py
@@ -1,16 +1,22 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
+grass_texture = load_texture('assets/grass_block.png')                            
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+
 class Voxel(Button):
     def __init__(self, position = (0,0,0)):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
+            model = 'assets/block',
             origin_y = 0.5,
-            texture = 'white_cube',
+            texture = grass_texture,
             color = color.color(0,0,random.uniform(0.9,1)),
-            highlight_color = color.lime)
+            scale = 0.5)
 
     def input(self,key):
         if self.hovered:
@@ -20,7 +26,7 @@ class Voxel(Button):
             if key == 'right mouse down':
                 destroy(self)
 
-app = Ursina()
+
 
 for z in range(20):
     for x in range(20):
